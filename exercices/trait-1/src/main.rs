f8ee481e09e0ad6155f8a30baa83d36821b6b246
Pierre Gabriel Tissot <pagehey@pm.me>
2022-05-13 11:20:34 +0200

    Resolve trait exercice

---
+++ exercices/trait-1/src/main.rs
@@ -0,0 +1,47 @@
+fn largest<T: PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    &largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let string_list = vec!["riri", "fifi", "loulou"];
+
+    let result = largest(&string_list);
+    println!("The largest string is {}", result);
+}
+
+// fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+
+// fn main() {
+//     let string1 = "short string";
+
+//     let result;
+//     let string2 = "this string is longer";
+
+//     result = longest(&string1, &string2);
+
+//     println!("this is the longest string: '{}'", result)
+// }
